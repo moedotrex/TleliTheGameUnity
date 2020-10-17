ed3eb0952a0e5d4abd2ce830ae4046986c4093f8
oblivioncow <72161732+oblivioncow@users.noreply.github.com>
2020-10-17 01:14:12 -0500

    Revert "Revert "cambios de escena""
    This reverts commit 70d0f3c80c4c0bea9c37b0f34a1ad63c972e4719.

---
+++ Assets/Tleli_TheGame/Scripts/Player/IkniCommand.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IkniCommand : MonoBehaviour
+{
+
+    TwinkyFollow ikniFollow;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ikniFollow = GameObject.FindGameObjectWithTag("Ikni").GetComponent<TwinkyFollow>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            if (ikniFollow.following == true)
+            {
+                ikniFollow.speed = 0f;
+                ikniFollow.following = false;
+                ikniFollow.Hold = true;
+            }
+            else if (ikniFollow.following == false)
+            {
+                ikniFollow.speed = 6f;
+                ikniFollow.following = true;
+                ikniFollow.Hold = false;
+            }
+        }
+    }
+}
