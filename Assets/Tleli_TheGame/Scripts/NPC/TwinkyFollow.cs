2d3c243bf3d36480d82a563a934d68ea998f1db4
oblivioncow <72161732+oblivioncow@users.noreply.github.com>
2020-10-17 01:11:36 -0500

    cambios de escena
    cambios de escena y agregacion de ikni command

--- Assets/Tleli_TheGame/Scripts/NPC/TwinkyFollow.cs
+++ Assets/Tleli_TheGame/Scripts/NPC/TwinkyFollow.cs
@@ -7,13 +7,40 @@ public class TwinkyFollow : MonoBehaviour
 {
     public float speed;
     public Transform TWaypoint;
+    public bool Hold;
+    public bool following;
+    public float tleliCloseness;
+
+    Transform target;
+
 
     void Start()
     {
+        following = true;
+        target = PlayerManager.instance.player.transform;
+
     }
 
     void Update()
     {
+
+        float distance = Vector3.Distance(target.position, transform.position);
+
+
+        if (distance >= tleliCloseness)
+        {
+            speed = 6f;
+            following = true;
+            Hold = false;
+        }
+
+
         transform.position = Vector3.MoveTowards(transform.position, TWaypoint.position, speed * Time.deltaTime);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, tleliCloseness);
     }
 }
