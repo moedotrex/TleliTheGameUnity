7c939ca08e9983cc37f0ae18777529f7b1b9d1ec
e1ker18 <a01702812@itesm.mx>
2020-10-16 21:03:48 -0400

    UnitTest_Folder for NPC Added by Emil

---
+++ Assets/Tleli_TheGame/Unit_Tests/SofEmil/Scripts/NPC_TestTest.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
+
+public class NPC_TestTest : MonoBehaviour
+{
+    //Button talk- Para presionar el botton 
+    //public GameObject buttonTalk;
+    public GameObject dialogue;
+    public Animator anim;
+    public GameObject helpText;
+    //Para la letra T
+    bool talk;
+    //Para rotar NPC hacia su Target
+    RotNPCTest rotNPC;
+
+    private Vector3 prevPosition;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        talk = false;
+        helpText.SetActive(false);
+        dialogue.SetActive(false);
+        rotNPC = GetComponent<RotNPCTest>();
+        //buttonTalk.SetActive(false); 
+    }
+
+    // Update is called once per frame
+   //Actualizando constantemente
+    void Update()
+    {
+        //Para que cuando se aprete la letra T se mande de botton a Dialogue
+        if (talk)
+        {
+            if (Input.GetKeyDown("t"))
+            {
+                Talk();
+            }
+
+        }
+
+       
+        
+    }
+
+    private void LateUpdate()
+    {
+
+        
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("Talking"))
+        {
+            rotNPC.TurnNPC();
+            rotNPC.TurnNPCHead();
+        }
+    }
+    //Aparezca el botton     
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player") 
+        {
+            talk = true;
+            helpText.SetActive(true);
+            //buttonTalk.SetActive(true);
+            anim.SetTrigger("Stand");
+            prevPosition = other.transform.position;
+            
+            
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player" && !other.transform.position.Equals(prevPosition))
+        {
+            rotNPC.StartRotation();
+            prevPosition = other.transform.position;
+        }
+    }
+    //Para que ya no aparezcan ninguno de los dos botones ni dialogo cuando sales
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            talk = false;
+            dialogue.SetActive(false);
+            helpText.SetActive(false);
+            //buttonTalk.SetActive(false);
+            anim.SetTrigger("Sit");
+
+        }
+    }
+    //Para mandar a llamar el dialogo a traves del botton
+    public void Talk()
+    {
+        dialogue.SetActive(true);
+        helpText.SetActive(false);
+        //buttonTalk.SetActive(false);
+
+    }
+}
---
+++ Assets/Tleli_TheGame/Unit_Tests/SofEmil/Scripts/RotNPCTest.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotNPCTest : MonoBehaviour
+
+{
+    public Transform head;
+
+    public Transform spine;
+    
+    public Transform target;
+
+    private float angularDelta = 0;
+
+    private Quaternion rotation;
+
+    public float minMaxAngle = 30;
+
+    public float rotSpeed=0.2f;
+
+
+    public void StartRotation()
+    {
+
+       
+        float dotProduct = Vector3.Dot(spine.forward, -target.forward);
+        print(dotProduct);
+        if(dotProduct>0)
+        {
+            rotation = Quaternion.LookRotation(-target.forward);
+        }
+        if (dotProduct <= 0)
+        {
+            rotation = Quaternion.identity;
+        }
+        //Buscar resolver por intervalos del valor de DotProduct
+        //Quaternion.Identity=Rotacion Nula
+        //LookRotation(-target.forward)
+        //LookRotation(target.forward)
+
+        //
+
+    }
+
+    public void TurnNPC()
+    {
+        spine.rotation = Quaternion.Lerp(spine.rotation, rotation, Time.deltaTime*rotSpeed);
+    }
+
+
+    /*public void StartRotation()
+    {
+        Vector3 targetPos = target.position;
+        Vector3 npcPos = spine.position;
+        angularDelta = Vector3.Angle(npcPos, targetPos);
+        angularDelta = Mathf.Clamp(angularDelta, -minMaxAngle, minMaxAngle);
+        Vector3 delta = new Vector3(targetPos.x - npcPos.x, 0.0f, targetPos.z - npcPos.z);;
+        rotation = Quaternion.LookRotation(delta);
+    }
+
+    public void TurnNPC()
+
+    {
+
+        //rotation.eulerAngles = new Vector3(rotation.eulerAngles.x, Mathf.Clamp(rotation.eulerAngles.y, -60, 60), rotation.eulerAngles.z);
+
+        spine.rotation = Quaternion.Slerp(spine.rotation, rotation, angularDelta * Time.deltaTime);   //Quaternion.RotateTowards(spine.rotation, rotation, 5f); 
+
+        
+        
+        
+        
+        
+        
+        //Vector3 direction= Vector3.Normalize(delta);
+
+        //Vector3 currentPos = npcPos;
+
+        //if(currentPos!=delta)
+        //{
+          //  currentPos = new Vector3(npcPos.x + 50 * direction.x, 0.0f, npcPos.z + 50 * direction.z);
+        //}
+
+        //While(Rot!=delta)
+         //{
+        //Rot= new Vector3(npcPos.x+Vector1, 0.0f,npcPos.z+Vector1)
+        //}
+        
+        //Vector1=(targetPos.x - npcPos.x)
+        
+
+        //This won't work with animated bones for some reason: Quaternion.Lerp(spine.rotation, rotation, 0.2f);
+
+    }*/
+
+    public void TurnNPCHead()
+
+    {
+        
+        //head.LookAt(target);
+
+    }
+
+}
