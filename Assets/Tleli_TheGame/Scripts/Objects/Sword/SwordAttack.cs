0750e94cf4939620e0ce5908b8dc69be802dad85
adrian-franco-tec <70407294+adrian-franco-tec@users.noreply.github.com>
2020-10-25 12:09:26 -0600

    incorporación de ataque secundario y sus versiones cargadas
    -cambios a SwordAttack.cs highlighteados con ///.
    -nuevas animaciones temporales y elementos en el arbol de animaciones.

--- Assets/Tleli_TheGame/Scripts/Objects/Sword/SwordAttack.cs
+++ Assets/Tleli_TheGame/Scripts/Objects/Sword/SwordAttack.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class SwordAttack : MonoBehaviour
 {
-
+    /// Adrián y Ciel
+   
     Animator animator;
 
     public float MArate = 0f;
@@ -21,6 +22,12 @@ public class SwordAttack : MonoBehaviour
 
     public float Damage;
 
+    ///
+    public float LAttackTime = 2;
+    public float HAttackTime = 2;
+    private float LAttackTimer = 0;
+    private float HAttackTimer = 0;
+    ///
 
     void Start()
     {
@@ -66,6 +73,83 @@ public class SwordAttack : MonoBehaviour
                 }
             }
         }
+
+        ///----------------------------------------------------------------
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (Time.time > ArateTimeStamp)
+            {
+
+                StartCoroutine(HAttackAnimation());
+                //FMODUnity.RuntimeManager.PlayOneShot(inputattacksound);
+                shootRay.origin = transform.position;
+                shootRay.direction = transform.forward;
+                ArateTimeStamp = Time.time + MArate;
+
+                Debug.DrawRay(transform.position, transform.forward, Color.red);
+
+
+                if (Physics.Raycast(shootRay, out hit, range, mask))
+                {
+
+                    EnemyHealth enemy = hit.transform.GetComponent<EnemyHealth>();
+                    EnemyController mov = hit.transform.GetComponent<EnemyController>();
+
+
+
+                    if (enemy != null)
+                    {
+                        enemy.HurtEnemy(Damage);
+                        mov.stopMov(0.2f);
+                    }
+                    if (hit.rigidbody != null)
+                    {
+
+                        hit.rigidbody.AddForce(-hit.normal * kBack);
+                    }
+
+                }
+            }
+        }
+        
+
+        if (Input.GetMouseButton(0))
+        {
+            LAttackTimer += Time.deltaTime;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            HAttackTimer += Time.deltaTime;
+        }
+
+        if (LAttackTimer >= LAttackTime && !Input.GetMouseButtonDown(1))
+        {
+            //UnityEngine.Debug.Log("LIGHT!");
+            animator.SetBool("Lcharge", true);
+        }
+
+        if (HAttackTimer >= HAttackTime)
+        {
+            //UnityEngine.Debug.Log("HEAVY!");
+            animator.SetBool("HCharge", true);
+        }
+
+        if (Input.GetMouseButtonUp(0) || Input.GetMouseButton(1))
+        {
+            //UnityEngine.Debug.Log("RELEASE!L");
+            animator.SetBool("Lcharge", false);
+            LAttackTimer = 0;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            //UnityEngine.Debug.Log("RELEASE H!");
+            animator.SetBool("HCharge", false);
+            HAttackTimer = 0;
+        }
+        ///----------------------------------------------------------------
     }
 
     IEnumerator AttackAnimation()
@@ -73,6 +157,14 @@ public class SwordAttack : MonoBehaviour
         animator.SetBool("isAttacking", true);
         yield return new WaitForSeconds(0.25f);
         animator.SetBool("isAttacking", false);
+
+    }
+
+    IEnumerator HAttackAnimation()
+    {
+        animator.SetBool("HAttack", true);
+        yield return new WaitForSeconds(0.25f);
+        animator.SetBool("HAttack", false);
 
     }
 }
