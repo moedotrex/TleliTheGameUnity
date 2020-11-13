dac772d71c2665640b4aed44d8e5cadad623c82c
e1ker18 <a01702812@itesm.mx>
2020-11-13 15:41:46 -0400

    Mouse Lock/Camera Sensitivity/Camera Angle Bug Fixes by Emil
    Now you can control the camera sensitivity with a public variable on the Main Camera object, and the Axis Controls from the camera are operated from within the CameraSensitivity script since it overwrites them.
    
    Mouse is locked on the screen (press ESC to free).

---
+++ Assets/Tleli_TheGame/Scripts/General/CameraSensitivity.cs
@@ -0,0 +1,30 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSensitivity : MonoBehaviour
+{
+    
+    public float cameraSensitivity = 2f;
+    CinemachineFreeLook freeLook;
+    private float ySensitivity;
+    private float xSensitivity; 
+
+    public void Start()
+    {
+        
+        freeLook = GameObject.FindGameObjectWithTag("CameraBrain").GetComponent<CinemachineFreeLook>();
+        ySensitivity = 1.5f * cameraSensitivity;
+        xSensitivity = 350f * cameraSensitivity;
+        SetCameraSensitivity();
+}
+
+    public void SetCameraSensitivity()
+    {
+        freeLook.m_XAxis= new AxisState(0.5f, ySensitivity, false, true, 0.1f, 0.1f, 0, "Mouse Y", false);
+        freeLook.m_XAxis = new AxisState(-180, 180, true, false, xSensitivity, 0.1f, 0.1f, "Mouse X", false);
+    }
+
+    
+}
