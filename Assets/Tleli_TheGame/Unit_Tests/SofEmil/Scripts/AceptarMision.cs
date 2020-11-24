4066b64c2ec7ff84d09f34c0bab8ae33a5dbb3c9
Sof Rodriguez <sofigrl@live.com>
2020-11-23 23:13:02 -0600

    NPC models

---
+++ Assets/Tleli_TheGame/Unit_Tests/SofEmil/Scripts/AceptarMision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AceptarMision : MonoBehaviour
+{
+    [Yarn.Unity.YarnCommand("AceptarMisionUno")]
+    public void AceptarMisionUno()
+    {
+        Debug.Log("Mision uno Aceptada");
+    }
+    [Yarn.Unity.YarnCommand("AceptarMisionDos")]
+    public void AceptarMisionDos()
+    {
+        Debug.Log("Mision dos Aceptada");
+    }
+
+}
