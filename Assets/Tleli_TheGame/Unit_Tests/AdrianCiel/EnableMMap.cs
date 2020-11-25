ed8cc26dea712592cfe9a580bac4f6fcbe37d074
ciel-tec <72561221+ciel-tec@users.noreply.github.com>
2020-11-24 20:53:10 -0600

    Map 2.0 (still needs fix)

--- Assets/Tleli_TheGame/Unit_Tests/AdrianCiel/EnableMMap.cs
+++ Assets/Tleli_TheGame/Unit_Tests/AdrianCiel/EnableMMap.cs
@@ -5,24 +5,47 @@ using UnityEngine.UI;
 
 public class EnableMMap : MonoBehaviour
 {
-    Image minimap;
-    Image marcador;
+    //Image minimap;
+    //Image marcador;
+    Vector3 TleliYPosition;
+    public GameObject MapToShow;
+    public GameObject UpperMap;
+    public GameObject LowerMap;
+
     // Start is called before the first frame update
     void Start()
     {
-        minimap = GameObject.Find("Mapa_0").GetComponent<Image>();
-        minimap.enabled = false;
-        marcador = GameObject.Find("Marcador").GetComponent<Image>();
-        marcador.enabled = false;
+        TleliYPosition = transform.position;
+        //minimap = GameObject.Find("Mapa_0").GetComponent<Image>();
+        //minimap.enabled = false;
+        //marcador = GameObject.Find("Marcador").GetComponent<Image>();
+        //marcador.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        TleliYPosition = transform.position;
         if (Input.GetKeyDown(KeyCode.M))
         {
-            minimap.enabled = !minimap.enabled;
-            marcador.enabled = !marcador.enabled;
+            //minimap.enabled = !minimap.enabled;
+            //marcador.enabled = !marcador.enabled;
+            MapToShow.SetActive(true);
+        }
+        if (TleliYPosition.y < 0f)
+        {
+            Debug.Log("Tleli esta abajo");
+            MapToShow = LowerMap;
+            UpperMap.SetActive(false);
+            MapToShow.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Tleli esta arriba");
+            MapToShow = UpperMap;
+            LowerMap.SetActive(false);
+            MapToShow.SetActive(true);
         }
     }
+
 }
