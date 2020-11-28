ebe29f22c7f7abfcc49b373c8211a7e13faebd68
Luis Peña -TEC <A01364029@itesm.mx>
2020-11-27 22:30:24 -0600

    AnimnationHeavy

--- Assets/Tleli_TheGame/Scripts/Enemy/HeavyAttack.cs
+++ Assets/Tleli_TheGame/Scripts/Enemy/HeavyAttack.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HeavyAttack : MonoBehaviour
 {
     public float timeBetweenAttacks = 3f;
     public float attackDamage = 1;
     public float attackDamageSlam = 1;
-    public float slamCounter = 0;
+    public float slamCounter = 20f;
     public float speed;
     public float jumpSpeed;
+    public float jumpRadio;
+    public bool isAnimating = false;
     
 
     private Vector3 slamLand;
     private Transform playerPos;
+
+    Transform target;
 
     GameObject player;
     //TlelliFlameHealth TlelliHealth;
@@ -21,6 +26,8 @@ public class HeavyAttack : MonoBehaviour
 
 
     tleliKnockBack playerKnockback;
+
+
 
     HeavyBoiAnimationController heavyBoiAnimationController; //moe 
 
@@ -31,6 +38,8 @@ public class HeavyAttack : MonoBehaviour
 
     void Start()
     {
+
+        target = PlayerManager.instance.player.transform;
         heavyBoiAnimationController = GetComponentInChildren<HeavyBoiAnimationController>(); //moe
 
         player = GameObject.FindGameObjectWithTag("Player");
@@ -47,117 +56,141 @@ public class HeavyAttack : MonoBehaviour
 
     void Update()
     {
-        timer += Time.deltaTime;
+        if (isAnimating == false)
+        {
+            timer += Time.deltaTime;
+        }
 
-            if (timer >= timeBetweenAttacks && playerInRange && isDisplaced == false )
+        if (timer >= timeBetweenAttacks && playerInRange && isDisplaced == false)
 
-       {
+        {
             int randomNum = Random.Range(1, 100);
-            slamCounter++;
+
             if (randomNum <= 66)
             {
-                Attack();
+                Debug.Log("nojala");
+
+                isAnimating = true;
+                timer = 0f;
+                heavyBoiAnimationController.LightAttackTrigger(); //moe
+                
+
+                
+
             }
 
             if (randomNum > 66)
             {
-                AttackSlam();
+                //heavyBoiAnimationController.LightAttackTrigger(); //moe
+                //timer = 0f;
 
             }
         }
 
-        if (timer >= timeBetweenAttacks && slamCounter >= 3)
+        float distance = Vector3.Distance(target.position, transform.position);
         {
-
-            JumpSlamp();
-            slamCounter = 0; 
-        }
-
-    }
-
-
-    void Attack()
-    {
-        timer = 0f;
-
-        if (TlelliHealth.HP > 0)
-        {
-            heavyBoiAnimationController.LightAttackTrigger(); //moe
-
-            TlelliHealth.HurtFlame(attackDamage);
-            playerKnockback.startKnockBack(5f);
-        }
-
-        if (TlelliHealth.flame <= 0)
-        {
-            TlelliHealth.SetHPDamage(1);
-        }
-        /* if (TlelliHealth.HP < 0)
-         {
-        Poner una barrera para que deje de atacar
-         }*/
-    }
-
-    void AttackSlam()
-    {
-        timer = 0f;
-
-        if (TlelliHealth.HP > 0)
-        {
-            heavyBoiAnimationController.LightAttackTrigger(); //moe
-
-            TlelliHealth.HurtFlame(attackDamage);
-            playerKnockback.startKnockBack(5f);
-        }
-
-        if (TlelliHealth.flame <= 0)
-        {
-            TlelliHealth.SetHPDamage(1);
+            if (distance <= jumpRadio && timer >= slamCounter && isDisplaced == true)
+            {
+                isAnimating = true;
+                timer = 0f;
+                heavyBoiAnimationController.JumpTrigger(); //moe 
+                JumpSlamp();
+            }
         }
     }
 
-    void JumpSlamp()
-    {
-        timer = 0f;
+         public void Attack()
+        {
+            timer = 0f;
+
+            if (TlelliHealth.flame > 0 && playerInRange)
+            {
+                
+                TlelliHealth.HurtFlame(attackDamage);
+                playerKnockback.startKnockBack(10f);
+            }
+
+            if (TlelliHealth.flame <= 0 && playerInRange)
+            {
+                TlelliHealth.SetHPDamage(1);
+            }
+            /* if (TlelliHealth.HP < 0)
+             {
+            Poner una barrera para que deje de atacar
+             }*/
+        }
+
+        public void AttackSlam()
+        {
+            timer = 0f;
+
+            if (TlelliHealth.flame > 0 && playerInRange)
+            {
+                
+
+                TlelliHealth.HurtFlame(attackDamage);
+                playerKnockback.startKnockBack(15f);
+            }
+
+            if (TlelliHealth.flame <= 0 && playerInRange)
+            {
+                TlelliHealth.SetHPDamage(1);
+            }
+        }
+
+        public void JumpSlamp()
+        {
 
         int randomNum = Random.Range(1, 100);
+
         if (TlelliHealth.HP > 0 && randomNum >= 80)
         {
+
             slamLand = new Vector3(playerPos.position.x, playerPos.position.y, playerPos.position.z);
             transform.position = Vector3.MoveTowards(transform.position, slamLand, jumpSpeed);
 
             if (playerInRange)
             {
-                heavyBoiAnimationController.JumpTrigger(); //moe
+
                 TlelliHealth.HurtFlame(attackDamage);
                 playerKnockback.startKnockBack(5f);
             }
+
+
+            if (TlelliHealth.flame <= 0)
+            {
+                TlelliHealth.SetHPDamage(1);
+            }
+
+
+        }
+        
+
+           
         }
 
-        if (TlelliHealth.flame <= 0)
+        //¿jugador en rango de ataque?
+        void OnTriggerEnter(Collider other)
         {
-            TlelliHealth.SetHPDamage(1);
+            if (other.gameObject == player)
+            {
+                playerInRange = true;
+            }
         }
-    }
 
-    //¿jugador en rango de ataque?
-    void OnTriggerEnter(Collider other)
+        //jugador fuera de rango
+        void OnTriggerExit(Collider other)
+        {
+            if (other.gameObject == player)
+            {
+                playerInRange = false;
+            }
+
+        }
+
+        void AnimationOff()
     {
-        if (other.gameObject == player)
-        {
-            playerInRange = true;
-        }
+        isAnimating = false;
     }
-
-    //jugador fuera de rango
-    void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject == player)
-        {
-            playerInRange = false;
-        }
-
-    }
-
     
 }
