ddc54940379acb935eab72b0e785fb7f51919b09
e1ker18 <a01702812@itesm.mx>
2020-12-04 17:42:01 -0400

    PushBox

---
+++ Assets/Tleli_TheGame/Scripts/General/PushBox.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBox : MonoBehaviour
+{
+    public Vector3 pushSpeed= new Vector3(1,0,1);
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.Translate(pushSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        other.transform.Translate(pushSpeed * Time.deltaTime);
+    }
+}
