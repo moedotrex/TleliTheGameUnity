5117f5f559f0d1559d718be0ac4cb349ec5f6579
carolinahuitc <70544035+carolinahuitc@users.noreply.github.com>
2020-12-05 00:27:25 -0600

    TUTORIAL - Dialogues TleliIkni Actualizados

--- Assets/Tleli_TheGame/Scripts/General/DialogueTleliIkni.cs
+++ Assets/Tleli_TheGame/Scripts/General/DialogueTleliIkni.cs
@@ -7,7 +7,9 @@ using UnityEngine.SceneManagement;
 public class DialogueTleliIkni : MonoBehaviour
 {
     public string[] dialogos;
+    public string[] personaje;
     private int dialogosIndex = 0;
+    public 
     bool TimeStarted = false;
     private float timer = 0f;
     public float pausaTime;
@@ -18,7 +20,6 @@ public class DialogueTleliIkni : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -46,6 +47,11 @@ public class DialogueTleliIkni : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Collider thisCollider = this.GetComponent<Collider>();
+
+            thisCollider.enabled = false;
+            changeDialogue();
+
+            /*
             switch (this.eventName)
             {
                 case "TleliIntro":
@@ -64,7 +70,7 @@ public class DialogueTleliIkni : MonoBehaviour
                     thisCollider.enabled = false;
                     changeDialogue();
                     break;
-                case "Event_FindIkni":
+                case "Dialogue_FindIkni":
                     thisCollider.enabled = false;
                     changeDialogue();
                     break;
@@ -73,13 +79,25 @@ public class DialogueTleliIkni : MonoBehaviour
                     changeDialogue();
                     break;
             }
-
+            */
         }
     }
 
     void changeDialogue()
     {
         taskText.text = dialogos[dialogosIndex];
+
+        Color tleliTextColor = new Color(1f, 0.7098039f, 0.4980392f, 1f);
+        Color ikniTextColor = new Color(0.4980392f, 0.8983116f, 1f, 1f);
+        switch (personaje[dialogosIndex])
+        {
+            case "T":
+                taskText.color = tleliTextColor;
+                break;
+            case "I":
+                taskText.color = ikniTextColor;
+                break;
+        }
         if (dialogosIndex < dialogos.Length)
         {
             if (!TimeStarted)
