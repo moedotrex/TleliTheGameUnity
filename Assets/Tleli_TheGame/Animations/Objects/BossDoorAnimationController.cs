2e430efd36b3bf41a98659afc6a48b7a0927c510
moedotrex <A01021974@itesm.mx>
2020-12-09 11:38:50 -0600

    Update BossDoorAnimationController.cs

--- Assets/Tleli_TheGame/Animations/Objects/BossDoorAnimationController.cs
+++ Assets/Tleli_TheGame/Animations/Objects/BossDoorAnimationController.cs
@@ -15,10 +15,7 @@ public class BossDoorAnimationController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("x"))
-        {
-            doorAnimator.SetTrigger("GotBossKeys");
-        }
+       
     }
 
     public void BossKeysTrigger()
