1cee88546246f3188c0352444e15dd2cf079c08e
Luis Peña -TEC <A01364029@itesm.mx>
2020-12-08 21:05:41 -0600

    3DSounds extras

---
+++ Assets/Tleli_TheGame/Scripts/Sonido/Emitter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+    [FMODUnity.EventRef]
+    public string selectSound;
+    FMOD.Studio.EventInstance soundEvent;
+    FMODUnity.StudioEventEmitter Sonido;
+
+    public string SonidoLight;
+   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundEvent = FMODUnity.RuntimeManager.CreateInstance(selectSound);
+
+        Sonido = gameObject.GetComponent<FMODUnity.StudioEventEmitter>();
+        Sonido.Play();
+
+    }
+}
