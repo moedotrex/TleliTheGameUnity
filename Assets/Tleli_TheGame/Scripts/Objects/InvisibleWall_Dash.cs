e887505057ab3013cf4b9cdd991b7292d4039dff
An <a01209942@itesm.mx>
2021-08-05 23:18:27 -0500

    Fix: Pared Invisible + tags de suelo

---
+++ Assets/Tleli_TheGame/Scripts/Objects/InvisibleWall_Dash.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWall_Dash : MonoBehaviour
+{
+    public PlayerDash Tleli;
+
+    void Update()
+    {
+
+        if(Tleli.gotDash == true)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
